73c655a4ef0983e3245e4f87695b1872810decc9
Mahmood787 <ali_khan87@live.com>
2020-11-19 11:44:41 +0500

    for user_metadata.full_name

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,13 +1,24 @@
-import React, {useEffect} from 'react'
-import {Button, Container, Flex, Heading} from 'theme-ui'
+import React, {useEffect, useState} from 'react'
+import {Button, Container, Flex, Heading, NavLink} from 'theme-ui'
 import netlifyIdentity from 'netlify-identity-widget'
+import {Link} from 'gatsby'
 
 export default props=> {
+    const [user, setUser]=useState()
     useEffect(()=>{
         netlifyIdentity.init({})
     })
+    netlifyIdentity.on("login", (user)=>{
+        netlifyIdentity.close();
+        setUser(user)
+    })
     return(
         <Container>
+            <Flex as="nav">
+                <NavLink as={Link} to="/" p={2}> Home</NavLink>
+                <NavLink as={Link} to="/app" p={2}> Dashboard</NavLink>
+                <NavLink  p={2}>{user.user_metadata.full_name} </NavLink>
+            </Flex>
             <Flex sx={{flexDirection:"column", padding:3}}>
                 <Heading as="h1">Hello World</Heading>
                 <Button 
